0e3d8202e869e2bbd434cefd93c46678c0d243f3
arusch <arusch@geninfo.com>
2017-01-22 17:16:47 -0500

    Added DAL Repository Interface and Class

---
+++ AdamsFirstMVC/DAL/IMandMREpository.cs
@@ -0,0 +1,35 @@
+﻿using AdamsFirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdamsFirstMVC.DAL
+{
+    interface IMandMRepository : IDisposable
+    {
+        IEnumerable<Collage> GetCollage();
+        Collage GetCollageByName(string collageName);
+        Collage GetCollageByID(int collageID);
+        void InsertCollage(Collage collage);
+        void DeleteCollage(int collageID);
+        void UpdateCollage(Collage collage);
+
+        IEnumerable<ClickableArea> GetClickableArea();
+        ClickableArea GetClickableAreaByName(string clickableAreaName);
+        ClickableArea GetClickableAreaByID(int clickableAreaID);
+        void InsertClickableArea(ClickableArea clickableArea);
+        void DeleteClickableArea(int clickableAreaID);
+        void UpdateClickableArea(ClickableArea clickableArea);
+
+        IEnumerable<BandImage> GetBandImage();
+        BandImage GetBandImageByName(string bandImageName);
+        BandImage GetBandImageByID(int bandImageID);
+        void InsertBandImage(BandImage bandImage);
+        void DeleteBandImage(int bandImageID);
+        void UpdateBandImage(BandImage bandImage);
+
+        void Save();
+    }
+}
---
+++ AdamsFirstMVC/DAL/MandMRepository.cs
@@ -0,0 +1,118 @@
+﻿using AdamsFirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AdamsFirstMVC.DAL
+{
+    public class MandMRepository : IMandMRepository, IDisposable
+    {
+        private MandMContext context;
+
+        public MandMRepository(MandMContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Collage> GetCollage()
+        {
+            return context.Collages.ToList();
+        }
+        public Collage GetCollageByName(string collageName)
+        {
+            return context.Collages.Find(collageName);
+        }
+        public Collage GetCollageByID(int collageID)
+        {
+            return context.Collages.Find(collageID);
+        }
+        public void InsertCollage(Collage collage)
+        {
+            context.Collages.Add(collage);
+        }
+        public void DeleteCollage(int collageID)
+        {
+            Collage collage = context.Collages.Find(collageID);
+            context.Collages.Remove(collage);
+        }
+        public void UpdateCollage(Collage collage)
+        {
+            context.Entry(collage).State = EntityState.Modified;
+        }
+
+        public IEnumerable<ClickableArea> GetClickableArea()
+        {
+            return context.ClickableAreas.ToList();
+        }
+        public ClickableArea GetClickableAreaByName(string clickableAreaName)
+        {
+            return context.ClickableAreas.Find(clickableAreaName);
+        }
+        public ClickableArea GetClickableAreaByID(int clickableAreaID)
+        {
+            return context.ClickableAreas.Find(clickableAreaID);
+        }
+        public void InsertClickableArea(ClickableArea clickableArea)
+        {
+            context.ClickableAreas.Add(clickableArea);
+        }
+        public void DeleteClickableArea(int clickableAreaID)
+        {
+            ClickableArea clickableArea = context.ClickableAreas.Find(clickableAreaID);
+            context.ClickableAreas.Remove(clickableArea);
+        }
+        public void UpdateClickableArea(ClickableArea clickableArea)
+        {
+            context.Entry(clickableArea).State = EntityState.Modified;
+        }
+
+        public IEnumerable<BandImage> GetBandImage()
+        {
+            return context.BandImages.ToList();
+        }
+        public BandImage GetBandImageByName(string bandImageName)
+        {
+            return context.BandImages.Find(bandImageName);
+        }
+        public BandImage GetBandImageByID(int bandImageID)
+        {
+            return context.BandImages.Find(bandImageID);
+        }
+        public void InsertBandImage(BandImage bandImage)
+        {
+            context.BandImages.Add(bandImage);
+        }
+        public void DeleteBandImage(int bandImageID)
+        {
+            BandImage bandImage = context.BandImages.Find(bandImageID);
+            context.BandImages.Remove(bandImage);
+        }
+        public void UpdateBandImage(BandImage bandImage)
+        {
+            context.Entry(bandImage).State = EntityState.Modified;
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+    }
+}

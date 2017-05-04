486151ca7836d02a848096054a8929d77c8db571
arusch <arusch@geninfo.com>
2017-05-04 04:13:14 -0400

    Developed the logic for Emails. Need to trouble shoot production error when sending...

--- AdamsFirstMVC/Controllers/MandMController.cs
+++ AdamsFirstMVC/Controllers/MandMController.cs
@@ -1,6 +1,10 @@
-﻿using AdamsFirstMVC.DAL;
+﻿using System;
+using System.Configuration;
+using System.Net;
+using AdamsFirstMVC.DAL;
 using System.Web.Mvc;
 using System.Net.Mail;
+using System.Text;
 using AdamsFirstMVC.Models;
 
 namespace AdamsFirstMVC.Controllers
@@ -47,6 +51,41 @@ namespace AdamsFirstMVC.Controllers
         {
             if (ModelState.IsValid)
             {
+                try
+                {
+                    MailMessage msg = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    StringBuilder sb = new StringBuilder();
+                    MailAddress from = new MailAddress(ConfigurationManager.AppSettings["smtpFromAddress"]);
+
+                    smtp.Host = ConfigurationManager.AppSettings["smtpHost"];
+                    smtp.EnableSsl =  Convert.ToBoolean(ConfigurationManager.AppSettings["smtpSsl"]);
+                    smtp.Port = Convert.ToInt16(ConfigurationManager.AppSettings["smtpPort"]);
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["smtpEmail"],
+                        ConfigurationManager.AppSettings["smtpPassword"]);
+
+                    sb.Append("First name: " + contact.FirstName);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Last name: " + contact.LastName);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Email: " + contact.Email);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Comments: " + contact.Comment);
+
+                    msg.To.Add(ConfigurationManager.AppSettings["smtpToAddresses"]);
+                    msg.From = from;
+                    msg.Subject = "Contact Us";
+                    msg.IsBodyHtml = false;
+                    msg.Body = sb.ToString();
+                    smtp.Send(msg);
+                    msg.Dispose();
+                    return View("Success");
+                }
+                catch (Exception e)
+                {
+                    return View("Error");
+                }
             }
             return View();
         }
--- AdamsFirstMVC/Models/Contact.cs
+++ AdamsFirstMVC/Models/Contact.cs
@@ -7,10 +7,14 @@ namespace AdamsFirstMVC.Models
     {
         [Required(ErrorMessage = "*First Name is Required")]
         public string FirstName { get; set; }
+
         [Required(ErrorMessage = "*Last Name is Required")]
         public string LastName { get; set; }
+
         [Required(ErrorMessage = "*Email is Required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
+
         [Required(ErrorMessage = "*A comment is Required")]
         public string Comment { get; set; }
     }
